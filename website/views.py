0f241256c185f829855ac40e7eef1f59dc8d74e3
saichimbili1 <59768898+saichimbili1@users.noreply.github.com>
2021-06-11 10:08:25 -0400

    Update views.py

--- website/views.py
+++ website/views.py
@@ -6,16 +6,16 @@ views = Blueprint('views',__name__)
 
 @views.route('/',methods=['POST','GET'])
 @login_required
-def home():
+def home():# This is the home page routing system seperate from the others
     if request.method =='POST':
         note = request.form.get('note')
-        if len(note)<1:
+        if len(note)<1: #ensures note is of right size
             flash("Note is too short",category="error")
         else:
             new_note = Note(data=note, user_id= current_user.id)
-       
+
             db.session.add(new_note)
-            db.session.commit()
-            
+            db.session.commit()# Adds the note information to the database
+
             flash("Note added",category="success")
     return render_template('home.html',user=current_user)
