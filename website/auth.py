2991e3a130c2eb4430476fd6f29b536119d269f5
saichimbili1 <59768898+saichimbili1@users.noreply.github.com>
2021-06-11 10:07:48 -0400

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -6,12 +6,12 @@ import time
 import smtplib
 auth = Blueprint('auth',__name__)
 @auth.route('/login', methods=['GET','POST'])
-def login():
+def login(): #Login features takes the credentials from the user and checks to see if it matches with existing database elements
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = Use.query.filter_by(email=email).first()
+        user = Use.query.filter_by(email=email).first()  # Distinguishes user by email firt
         if user:
             print(user.password)
             if (user.password==password):
@@ -33,11 +33,11 @@ def logout():
 
 @auth.route('/com',methods=['GET','POST'])
 @login_required
-def com():
+def com(): # This posts the information on the community post section
     s = Use.query.all()
-    usr = Community.query.all()
+    usr = Community.query.all() # This quiries and stores all the information so that it can be posted
     if request.method=='GET':
-        return render_template('comm.html',user=current_user,usr=usr) 
+        return render_template('comm.html',user=current_user,usr=usr,Use=s)
     if request.method=='POST':
         usr = Community.query.all()
         cNote= request.form.get("Cnote")
@@ -50,25 +50,25 @@ def com():
             pers = new_note.user_id
             db.session.add(new_note)
             db.session.commit()
-            flash("Note added",category="success") 
+            flash("Note added",category="success")
             usr = Community.query.all()
 
-            
 
 
-    return render_template('comm.html',user=current_user,usr=usr,pers = pers,Use=s)      
+
+    return render_template('comm.html',user=current_user,usr=usr,pers = pers,Use=s)
 
 
 
 
 @auth.route('/sign-up',methods=['GET','POST'])
-def sign_up():
-    maybe = False
+def sign_up(): # Allows user to sign up and enter their information into the database
+
     if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+        password2 = request.form.get('password2')  #These requests take information from HTML forms
         print(password1)
         user = Use.query.filter_by(email=email).first()
         if user:
@@ -80,7 +80,7 @@ def sign_up():
         elif password1!=password2:
             flash('Passwords do not match',category='error')
         else:
-           
+
             new_user = Use(email=email,first_name =firstName, password=password1)
             db.session.add(new_user)
             db.session.commit()
@@ -91,21 +91,23 @@ def sign_up():
 
 
 @auth.route('/forgot_pass',methods=['GET','POST'])
-def forgot_pass():
+def forgot_pass():# returns password for those who forgot their passwords
     if request.method =="POST":
-        sender_email = "Sai.chimbili@gmail.com"
+        sender_email = "csforum1230@gmail.com"
         recEM = request.form.get("emailF")
-        password= "Sai123Rakshith"
-        user = Use.query.filter_by(email=recEM).first()
+        password= "YOLO1234"
+        user = Use.query.filter_by(email=recEM).first() # Isolates the password by using the email given to filter
         if user:
             message = user.password
             server = smtplib.SMTP('smtp.gmail.com',587)
             server.starttls()
             server.login(sender_email, password)
-            server.sendmail(sender_email,recEM,message)
+            server.sendmail(sender_email,recEM,message)  # Sends an email to the given email to recover password
+
+            flash("Password Sent to Email",category="success")
         else:
             flash("user does not exist",category="error")
 
-       
+
 
     return render_template('forgot_pass.html',user=current_user)
